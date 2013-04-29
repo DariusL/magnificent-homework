4a7541178ab48ef2e382dd8880b0b5b1d53cd8ea
DariusL <d.lapunas@gmail.com>
2013-04-29 23:58:04 +0300

    actual initial commit

---
+++ taskai mk 1/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿using System.Drawing;
+namespace taskai_mk_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Location = new System.Drawing.Point(663, 166);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(314, 394);
+            this.panel1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1092, 745);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+            panel1.Paint += panel1_Paint;
+        }
+
+        void panel1_Paint(object sender, System.Windows.Forms.PaintEventArgs e)
+        {
+            Graphics g = panel1.CreateGraphics();
+            Pen p = new Pen(Color.Red, 5);
+            g.DrawLine(p, new Point(5, 5), new Point(40, 40));
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+
+    }
+}
+
